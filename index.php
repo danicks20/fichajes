f6d02dd440136c8858b6912569222ac2c343993c
Daniel <daniel.gperez@outlook.es>
2021-11-30 22:26:23 +0100

    Añadir estilos e imagenes

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <title>Software fichajes</title>
     <!-- Compressed CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.4/dist/css/foundation.min.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="/static/css/estilos.css">
+    <link href="/fichajes/static/libs/fontawesome5/css/all.css" rel="stylesheet"> <!--load all styles -->
+    <link rel="stylesheet" href="/fichajes/static/css/estilos.css">
 </head>
 <body>
 
@@ -31,31 +32,73 @@
                 <div class="cell text-center">
                     <h1>Software fichajes</h1>
                 </div>
-                <fieldset class="cell">
+                <fieldset class="cell text-center callout">
                     <legend><strong>Seleccione la empresa</strong></legend>
                     
-                    <input type="radio" name="empresa" id="empresaKike" value="Kike" required
-                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre']) echo 'disabled'; ?> 
-                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre'] == "Kike") echo 'checked'; ?> 
-                    ><label for="empresaKike">Kike</label>
-                    
-                    <input type="radio" name="empresa" id="empresaEdu" value="Edu"
-                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre']) echo 'disabled'; ?> 
-                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre'] == "Edu") echo 'checked'; ?> 
-                    ><label for="empresaEdu">Edu</label>
+                    <div class="grid-x grid-padding-x">
+                        <div class="cell medium-4 text-center">
+                            <label for="empresaKike">
+                                <!-- image has no padding -->
+                                <div class="card align-center-middle">
+                                    <img src="static/img/Carlos.PNG">
+                                    <div class="card-section text-center">
+                                        <p>Carlos Bueno peritaciones</p>
+                                    </div>
+                                </div>
+                            </label>
+                            <div>
+                                <input type="radio" name="empresa" id="empresaKike" value="Kike" required
+                                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre']) echo 'disabled'; ?> 
+                                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre'] == "Kike") echo 'checked'; ?> 
+                                >
+                            </div>
+                        </div>
 
-                    <input type="radio" name="empresa" id="empresaEmilio" value="Emilio"
-                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre']) echo 'disabled'; ?> 
-                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre'] == "Emilio") echo 'checked'; ?>
-                    ><label for="empresaEmilio">Talleres Emilio</label>
+                        <div class="cell medium-4 text-center">
+                            <label for="empresaEdu">
+                                <!-- image has no padding -->
+                                <div class="card align-center-middle">
+                                    <img src="static/img/Taxa.PNG">
+                                    <div class="card-section text-center">
+                                        <p>Taxa peritaciones</p>
+                                    </div>
+                                </div>
+                            </label>
+                            <div>
+                                <input type="radio" name="empresa" id="empresaEdu" value="Edu"
+                                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre']) echo 'disabled'; ?> 
+                                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre'] == "Edu") echo 'checked'; ?> 
+                                >
+                            </div>
+                        </div>
+
+                        <div class="cell medium-4 text-center">
+                            <label for="empresaEmilio">
+                                <!-- image has no padding -->
+                                <div class="card align-center-middle">
+                                    <img src="static/img/Emilio.PNG">
+                                    <div class="card-section text-center">
+                                        <p>Talleres Emilio</p>
+                                    </div>
+                                </div>
+                            </label>
+                            <div>
+                                <input type="radio" name="empresa" id="empresaEmilio" value="Emilio"
+                                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre']) echo 'disabled'; ?> 
+                                    <? if(isset($_SESSION['pdte_fichar_cierre']) && $_SESSION['pdte_fichar_cierre'] == "Emilio") echo 'checked'; ?>
+                                >
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="cell">
+                        <? if ((new RegistroHora)->cierrePending()): ?>
+                            <button type="submit" class="button alert" name="ficha_salida">Fichar salida <i class="fas fa-sign-out-alt"></i></button>
+                        <? else: ?>
+                            <button type="submit" class="button" name="ficha_entrada"><i class="fas fa-sign-in-alt"></i> Fichar entrada</button>
+                        <? endif; ?>
+                    </div>
                 </fieldset>
-                <div class="cell">
-                    <? if ((new RegistroHora)->cierrePending()): ?>
-                        <button type="submit" class="button" name="ficha_salida">Fichar salida</button>
-                    <? else: ?>
-                        <button type="submit" class="button" name="ficha_entrada">Fichar entrada</button>
-                    <? endif; ?>
-                </div>
             </div>
         </div>
     </form>
@@ -64,8 +107,14 @@
 
    <div class="grid-container">
         <div class="grid-x grid-padding-x">
-            <div class="cell text-center"><?= strftime("%A %d de %B del %Y"); ?></div>
-            <div class="cell text-center" style="margin-top: 10px;"><strong>Tiempo total: </strong><?= (new RegistroHora)->getTotalTime(); ?> horas.</div>
+            <div class="cell text-center">
+                <i class="far fa-calendar-alt"></i>
+                <?= strftime("%A %d de %B del %Y"); ?>
+            </div>
+            <div class="cell text-center" style="margin-top: 10px;">
+                <i class="far fa-clock"></i>
+                <strong>Tiempo total: </strong><?= (new RegistroHora)->getTotalTime(); ?> horas.
+            </div>
             <div class="cell text-center" style="margin-top: 50px;">
                 <table class="hover">
                     <thead>
